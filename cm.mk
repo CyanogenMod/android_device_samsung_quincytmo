b48e4859056e0e26c1f413d56488e5f61c25f0da
Steve Kondik <shade@chemlab.org>
2012-08-21 02:37:43 -0700

    Initial import based on quincyatt
     * It's the same device, changes are minimal.

---
+++ cm.mk
@@ -0,0 +1,18 @@
+$(call inherit-product, device/samsung/quincytmo/full_quincytmo.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T879 TARGET_DEVICE=SGH-T879 BUILD_FINGERPRINT="samsung/SGH-T879/SGH-T879:4.0.4/IMM76D/UVLE9:user/release-keys" PRIVATE_BUILD_DESC="SGH-T879-user 4.0.4 IMM76D UVLE9 release-keys"
+
+TARGET_BOOTANIMATION_NAME := vertical-800x1280
+
+PRODUCT_NAME := cm_quincytmo
+PRODUCT_DEVICE := quincytmo
+
